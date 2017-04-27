851129825e25dc7ab43d34110d7e6872ba8a22e3
mfvetter <mvetter@MV-MacBook-Pro.local>
2017-04-27 13:19:17 -0700

    Shows Telephone Tab last
    After I installed the extensions, the Telephone tab was the first and
    not Page. This makes the Telephone Tab be the last, after Email.

--- Configuration/TSconfig/Page/LinkHandler.ts
+++ Configuration/TSconfig/Page/LinkHandler.ts
@@ -2,7 +2,6 @@ TCEMAIN.linkHandler {
     tel {
         handler = Monosize\LinkHandler\Plus\Recordlist\LinkHandler\TelLinkHandler
         label = LLL:EXT:linkhandler_plus/Resources/Private/Language/locallang_browse_links.xlf:tel
-        displayBefore = url
-        scanBefore = url
+        scanAfter = page
     }
 }
